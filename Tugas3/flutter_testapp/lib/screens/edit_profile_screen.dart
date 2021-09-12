33d92bc83af88a74efaf62bb97ad8a567031cb02
rafliandi13 <herp1306@gmail.com>
2021-09-13 00:19:28 +0700

    Penambahan Validation di CheckBox dan RadioButton

--- Tugas3/flutter_testapp/lib/screens/edit_profile_screen.dart
+++ Tugas3/flutter_testapp/lib/screens/edit_profile_screen.dart
@@ -8,26 +8,21 @@ enum SingingCharacter { lakilaki, perempuan }
 class _EditProfileScreenState extends State<EditProfile> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   bool isChecked = false;
-  bool dalamnegri = false;
-  bool luarnegri = false;
-
+  bool Facebook = false;
+  bool Twitter = false;
+  bool Linkedin = false;
   SingingCharacter? _character ;
   final Controller2 = TextEditingController();
   final Controller1 = TextEditingController();
   final Controller3 = TextEditingController();
-  int _counter = 0;
+  String? media;
   var formatter = new DateFormat('yyyy-MM-dd');
   DateTime fromDate =
   DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
   DateTime toDate =
   DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+  bool showErrorMessage = false;
+  bool showErrorMessageRadioButton = false;
   Future<DateTime> selectDate(BuildContext context, DateTime _date) async {
     final DateTime? picked = await showDatePicker(
       context: context,
@@ -137,29 +132,48 @@ class _EditProfileScreenState extends State<EditProfile> {
                               return null;
                             },
                           ),
+                          SizedBox(height: 10),
+                          Text("Social Media Yang Dipunya : "),
+                          CheckboxListTile(
+                            title: Text('Facebook'),
+                            value: Facebook,
+                            onChanged: (bool? value) {
+                              setState(() {
+                                Facebook = value!;
+                              });
+                            },
+                          ),
+                          CheckboxListTile(
+                            title: Text('Twitter'),
+                            value: Twitter,
+                            onChanged: (bool? value) {
+                              setState(() {
+                                Twitter = value!;
+                              });
+                            },
+                          ),
+                          CheckboxListTile(
+                            title: Text('Linkedin'),
+                            value: Linkedin,
+                            onChanged: (bool? value) {
+                              setState(() {
+                                Linkedin = value!;
+                              });
+                            },
+                          ),
+                          showErrorMessage ?
+                          Container(
+                              child: Padding(
+                                  padding: EdgeInsets.all(10.0),
+                                  child: Text('Pilih CheckBox!',style: TextStyle(color: Colors.red),)
+                              )
+                          )
+                              : Container(),
                         ],
                       )
                   ),
 
-                  SizedBox(height: 10),
-                  CheckboxListTile(
-                    title: Text('Dalam Negri'),
-                    value: dalamnegri,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        dalamnegri = value!;
-                      });
-                    },
-                  ),
-                  CheckboxListTile(
-                    title: Text('Luar Negri'),
-                    value: luarnegri,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        luarnegri = value!;
-                      });
-                    },
-                  ),
+
                   Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
@@ -195,6 +209,14 @@ class _EditProfileScreenState extends State<EditProfile> {
                       },
                     ),
                   ),
+                  showErrorMessageRadioButton ?
+                  Container(
+                      child: Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Text('Pilih Salah Satu !',style: TextStyle(color: Colors.red),)
+                      )
+                  )
+                      : Container(),
                   Padding(
                     padding: const EdgeInsets.symmetric(vertical: 16.0),
                     child: ElevatedButton(
@@ -208,7 +230,7 @@ class _EditProfileScreenState extends State<EditProfile> {
                       onPressed: () {
                         // Validate will return true if the form is valid, or false if
                         // the form is invalid.
-                        String? media;
+
                         switch (_character) {
                           case SingingCharacter.lakilaki:
                             media = 'Laki - Laki';
@@ -219,31 +241,44 @@ class _EditProfileScreenState extends State<EditProfile> {
                         }
                         if (_formKey.currentState!.validate()) {
                           // Process data.
-                          showDialog(
-                            context: context,
-                            builder: (context) {
-                              return AlertDialog(
-                                // Retrieve the text the that user has entered by using the
-                                // TextEditingController.
-                                title: const Text('Apakah Anda Ingin Menyimpan Data ?'),
-                                content: Text('Nama : ' + Controller1.text+'\n' + 'Nama Pengguna: ' + Controller2.text +'\n'
-                                    'Bio : ' + Controller1.text + '\n'+ (dalamnegri ? 'Dalam Negri\n' : '') + (luarnegri ? 'Luar Negri\n' : '') +
-                                    'Tanggal Lahir : ' +'${formatter.format(fromDate)}\n' +
-                                    'Gender : ' + media!
-                                ),
-                                actions: [
-                                  TextButton(
-                                  child: Text("Cancel"),
-                                    onPressed: () => Navigator.pop(context, 'Cancel'),
+                          if ( (Facebook != true && Twitter != true && Linkedin != true)){
+                            setState(() => showErrorMessage = true);
+                          }
+                          else if(media == null){
+                            setState(() => showErrorMessageRadioButton = true);
+                            setState(() => showErrorMessage = false);
+                          }
+                          else{
+                            setState(() => showErrorMessageRadioButton = false);
+                            setState(() => showErrorMessage = false);
+                            showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  // Retrieve the text the that user has entered by using the
+                                  // TextEditingController.
+                                  title: const Text('Apakah Anda Ingin Menyimpan Data ?'),
+                                  content: Text('Nama : ' + Controller1.text+'\n' + 'Nama Pengguna: ' + Controller2.text +'\n'
+                                      'Bio : ' + Controller1.text + '\n'+ 'Social Media : '+(Facebook ? 'Facebook' : '') + (Twitter ? ',Twitter' : '') +
+                                      (Linkedin ? ',Linkedin' : '') +
+                                      '\nTanggal Lahir : ' +'${formatter.format(fromDate)}\n' +
+                                      'Gender : ' + media!
                                   ),
-                                  TextButton(
-                                  child: Text("Ok"),
+                                  actions: [
+                                    TextButton(
+                                      child: Text("Cancel"),
+                                      onPressed: () => Navigator.pop(context, 'Cancel'),
+                                    ),
+                                    TextButton(
+                                      child: Text("Ok"),
                                       onPressed: () => Navigator.pop(context, 'Ok'),
-                                  )
-                                ],
-                              );
-                            },
-                          );
+                                    )
+                                  ],
+                                );
+                              },
+                            );
+                          }
+
                         }
                       },
                       child: const Text('Simpan'),
